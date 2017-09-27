7d70b0ab38b37f6d597c0c1f9483b199e78f7126
Frederik Martens <frederik.martens@gmail.com>
2017-09-27 23:20:04 +0200

    First code and tests for anagrams kata. Needs refactoring and extra code

---
+++ src/anagrams/Anagrams.java
@@ -0,0 +1,14 @@
+package anagrams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Anagrams {
+	public static List<String> of(String s) {
+		if (s.equals("")) {
+			return new ArrayList<>();
+		}
+		return new ArrayList<>(Arrays.asList(s));
+	}
+}
---
+++ test/anagrams/AnagramsTest.java
@@ -0,0 +1,25 @@
+package anagrams;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class AnagramsTest {
+
+	@Test
+	public void noCharacters() throws Exception {
+		List<String> emptyListOfString = new ArrayList<>();
+		assertThat(Anagrams.of(""), equalTo(emptyListOfString));
+	}
+
+	@Test
+	public void oneCharacter() throws Exception {
+		ArrayList<String> expected = new ArrayList<>(Arrays.asList("a"));
+		assertThat(Anagrams.of("a"), equalTo(expected));
+	}
+}
