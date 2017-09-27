a9c9b974e9ab0f6d75237da6e806d8071f264431
Frederik Martens <frederik.martens@gmail.com>
2017-09-27 23:24:33 +0200

    removed commented code

--- src/anagrams/Anagrams.java
+++ src/anagrams/Anagrams.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 public class Anagrams {
 	public static List<String> of(String s) {
-//		if (s.equals("")) {
-//			return new ArrayList<>();
-//		}
 		return new ArrayList<>(Arrays.asList(s));
 	}
 }
